dfcdf3e6bb193003b7120054177071d3ac5b39fc
Maxim Butenko <maxim.butenko.drive@gmail.com>
2024-04-21 05:01:39 +0400

    fix client url

--- apps/client/src/api/base.ts
+++ apps/client/src/api/base.ts
@@ -5,7 +5,9 @@ export class BaseApiClient {
     protected headers: Record<string, string> = {};
 
     constructor() {
-        const baseURL = import.meta.env.DEV ? 'http://localhost:3000/api' : '';
+        const baseURL = import.meta.env.DEV
+            ? 'http://localhost:3000/api'
+            : '/api';
         this.client = axios.create({ baseURL });
     }
 
